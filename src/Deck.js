c6798e68795c524f2bf418a3b92612b6ac2b2732
Magda Ambrozy <magda.ambrozy@gmail.com>
2020-01-16 18:41:12 +0100

    Add api requests in Deck.js and set state

--- src/Deck.js
+++ src/Deck.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+const BASE_URL = 'https://deckofcardsapi.com/api/deck/';
 
 class Deck extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
+    state = {
             deck: null,
+            drawn: []
         }
+    
+    async componentDidMount(){
+        let deck = await axios.get(`${BASE_URL}/new/shuffle/?deck_count=1`);
+        this.setState({ deck: deck.data});
     }
 
-    async componentDidMount(){
-        let deck = await axios.get(API_URL);
-        this.setState({ deck: deck.data});
+    getCard = async () => {
+        let id = this.state.deck.deck_id;
+        let cardURL = `${BASE_URL}${id}/draw/?count=1`;
+        //make request using cardURL
+        try {
+            let cardRes = await axios.get(cardURL);
+
+            if(!cardRes.data.success) {
+                throw new Error("No cards remaining!");
+            }
+
+            //set state using new card info from api
+            let card = cardRes.data.cards[0];
+            console.log(card);
+            this.setState(st => ({
+                drawn: [
+                    ...st.drawn,
+                    {
+                        id: card.code,
+                        image: card.image,
+                        name: `${card.value} of ${card.suit} `
+                    }
+                ]
+            }));
+        } catch (err){
+            alert(err);
+        }
+        
     }
 
     render() {
         return (
             <div>
                 <h1> Card Dealer </h1>
+                <button onClick={this.getCard}>Get card!</button>
             </div>
         );
     }
